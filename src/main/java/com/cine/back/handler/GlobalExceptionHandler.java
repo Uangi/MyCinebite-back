aa8edfc0959a91c48d08b2b0587a58adc7f0ed2d
Uangi <jyp423@naver.com>
2024-06-21 00:26:32 +0900

    Add : 예외처리 (이미 평가한 영화) 추가

--- src/main/java/com/cine/back/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cine/back/handler/GlobalExceptionHandler.java
@@ -5,6 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.cine.back.movieList.exception.AlreadyEvaluatedException;
+import com.cine.back.movieList.exception.MovieNotFoundException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     /**
@@ -12,7 +15,9 @@ public class GlobalExceptionHandler {
      * required : HttpStatus, Exception e
      * @ExceptionHandler()
      */
-
+    private static final int NOT_FOUND_ERROR = 404;
+    private static final int CONFILCT_ERROR = 409;
+    
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류 " + e.getMessage());
@@ -21,6 +26,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("잘못된 요청 " + e.getMessage());
+    }
+
+    @ExceptionHandler(MovieNotFoundException.class)
+    public ResponseEntity<?> handleMovieNotFoundException(MovieNotFoundException e) {
+        return ResponseEntity.status(NOT_FOUND_ERROR).body(e.getMessage());
+    }
+
+    @ExceptionHandler(AlreadyEvaluatedException.class)
+    public ResponseEntity<?> handleAlreadyEvaluatedException(AlreadyEvaluatedException e) {
+        return ResponseEntity.status(CONFILCT_ERROR).body(e.getMessage());
     }
 
 }
